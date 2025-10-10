60d9aec81c67094fd7b95f90d8a85192212ec6c2
harishammache <hammache_haris@hotmail.fr>
2025-10-10 17:09:20 +0200

    fix: update year

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -2,8 +2,6 @@ import './App.css';
 import logo from './assets/holberton-logo.jpg';
 
 function App() {
-  const currentYear = new Date().getFullYear();
-
   return (
     <div className="App">
       <div className="App-header">
@@ -16,7 +14,7 @@ function App() {
       </div>
 
       <div className="App-footer">
-        <p>Copyright {currentYear} - Holberton School</p>
+        <p>Copyright 2025 - Holberton School</p>
       </div>
     </div>
   );
