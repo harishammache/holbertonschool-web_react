0bf1b350c04f5b32dd02c00b7f27c74e82ea82e2
harishammache <hammache_haris@hotmail.fr>
2025-10-17 13:37:33 +0200

    refactor: simplify App component structure and improve formatting

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,23 +1,23 @@
-import './App.css';
+import './App.css'
 import logo from './assets/holberton-logo.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="holberton logo" />
-        <h1>School dashboard</h1>
-      </div>
-
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-      </div>
-
-      <div className="App-footer">
-        <p>Copyright 2025 - holberton School</p>
-      </div>
+    <>
+    <div className="App-header">
+      <img alt="holberton logo" src={logo}></img>
+      <h1>School dashboard</h1>
     </div>
-  );
+
+    <div className="App-body">
+      <p>Login to access the full dashboard</p>
+    </div>
+
+    <div className="App-footer">
+      <p>Copyright 2025 - holberton School</p>
+    </div>
+    </>
+  )
 }
 
-export default App;
+export default App
