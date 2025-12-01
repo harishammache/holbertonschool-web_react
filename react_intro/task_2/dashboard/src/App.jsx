b65880cd9f80a9e8c9d587289982b9c70613b495
harishammache <hammache_haris@hotmail.fr>
2025-12-01 14:48:49 +0100

    refactor: restructure App component layout and improve copyright display

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -5,12 +5,14 @@ import Notifications from './Notifications';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <div className="root-notifications">
+        <Notifications />
+      </div>
       <div className="App-header">
-        <img src={logo} className="App-logo" alt="holberton logo" />
+        <img src={logo} alt="holberton logo" />
         <h1>School dashboard</h1>
       </div>
-
       <div className="App-body">
         <p>Login to access the full dashboard</p>
         <form>
@@ -21,15 +23,10 @@ function App() {
           <button type="button">OK</button>
         </form>
       </div>
-
-      <div className="root-notifications">
-        <Notifications />
-      </div>
-
       <div className="App-footer">
-        <p>Copyright {getCurrentYear} - {getFooterCopy}</p>
+        <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
       </div>
-    </div>
+    </>
   );
 }
 
